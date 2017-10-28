f43a41cc9d40a39300bd20c6eff1d81ef8e66067
Karl Casserfelt <karl.casserfelt@gmail.com>
2017-10-29 01:10:43 +0200

    worked on median calculation

--- src/Main.java
+++ src/Main.java
@@ -3,14 +3,22 @@ import org.opencv.core.Core;
 import org.opencv.core.CvType;
 import org.opencv.imgproc.*;
 import org.opencv.core.Mat;
+import org.opencv.core.MatOfPoint;
+import org.opencv.core.Point;
+import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
 import org.opencv.core.Size;
 import org.opencv.highgui.VideoCapture;
 import org.opencv.video.BackgroundSubtractorMOG;
 import org.opencv.video.BackgroundSubtractorMOG2;
 
-
+import java.awt.List;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
 
 import org.opencv.*;
+import org.opencv.imgproc.*;
 
 /**
  * Reads and analyzes images
@@ -32,11 +40,13 @@ public class Main {
 
 	public Main() {
 		
-//		while (true){
-//			getDiff();
-//		}
+		while (true){
+			getDiff();
+		}
 		
-		backgroundSubtractionTest();
+//		backgroundSubtractionTest();
+		
+	//	medianBackgroundTest();
 		
 	}
 	
@@ -46,8 +56,8 @@ public class Main {
 		Mat diffImage = new Mat(newImage.height(), newImage.width(), CvType.CV_8UC3);
 		Mat thresImage = new Mat(newImage.height(), newImage.width(), CvType.CV_8UC3);
 
-		int threshold = 40;
-		int blurSize = 10;
+		int threshold = 20;
+		int blurSize = 50;
 
 		Imgproc.cvtColor(newImage, newImageGray, Imgproc.COLOR_RGB2GRAY);		
 		
@@ -57,10 +67,31 @@ public class Main {
 			Imgproc.threshold(diffImage, thresImage, threshold, 255, Imgproc.THRESH_BINARY);
 			
 			Imgproc.blur(thresImage, thresImage, new Size(blurSize, blurSize));
+			
+			//TODO: check out Core.adaptiveThreshold for this later
 			Imgproc.threshold(thresImage, thresImage, threshold, 255, Imgproc.THRESH_BINARY);
-
-		
-
+			
+			
+			ArrayList<MatOfPoint> contours = new ArrayList<MatOfPoint>();    
+			
+		    Imgproc.findContours(thresImage, contours, new Mat(), Imgproc.RETR_EXTERNAL,Imgproc.CHAIN_APPROX_SIMPLE);
+		    
+		    
+		    boolean objectDetected = false;
+		    
+		    if (contours.size() > 0){
+		    	objectDetected = true;
+		    }
+		    
+		    if (objectDetected){
+		    	
+		    	for(int i=0; i< contours.size();i++){
+		            if (Imgproc.contourArea(contours.get(i)) > 50 ){
+		                Rect rect = Imgproc.boundingRect(contours.get(i));
+		                Core.rectangle(newImage, new Point(rect.x,rect.height), new Point(rect.y,rect.width),new Scalar(0,0,255));
+		            }
+		    	}
+		    }
 		}
 		
 		lastImage = newImageGray;
@@ -86,7 +117,59 @@ public class Main {
 			Display.displayImage(mask);
 			
 		}
+	}
+	
+	
+	@SuppressWarnings("unchecked")
+	private static void medianBackgroundTest(){
 		
+		LinkedList<Mat> fifo = new LinkedList<>();
+		int populationSize = 50;
+		
+		for (int i = 0; i < populationSize; i ++){
+			fifo.push(getNextImage());
+		}
+		
+		Mat img = fifo.peek();
+		int rows = img.rows();
+		int cols = img.cols();
+		
+		int resizeRows = (int) (rows * 0.1);
+		int resizeCols = (int) (cols * 0.1);
+		Size smallSize = new Size(resizeRows, resizeRows);
+		Mat smallImage = new Mat();
+		
+		// Resize and gray scale image
+		Imgproc.resize(img, smallImage, smallSize);
+		Imgproc.cvtColor(img, img, Imgproc.COLOR_RGB2GRAY);		
+
+		// create store for saving values to compute 
+		LinkedList<Double>[][] valueStores = new LinkedList[resizeRows][resizeCols];
+		
+		for (int row = 0; row < resizeRows; row++){
+			for (int col = 0; col < resizeCols; col++){
+				double[] intensityData = img.get(row, col);
+				double data = intensityData[0];
+				valueStores[row][col].push(data);				
+			}
+		}
+		
+		
+		
+		// Later
+		Mat medianBackground = new Mat(resizeCols, resizeRows, CvType.CV_8UC3);
+		for (int i = 0; i < resizeRows; i++){
+			for(int j = 0; j < resizeCols; j++){
+				
+				// sort pixel values for position
+				Collections.sort(valueStores[i][j]);
+				// add median value at position
+				medianBackground.put(i, j, valueStores[i][j].remove(valueStores[i][j].size() / 2));
+				
+				
+			}
+			
+		}
 		
 		
 		
